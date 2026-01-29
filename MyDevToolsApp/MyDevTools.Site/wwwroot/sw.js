28c72aa3b769a78affde962310f937687bdb11a4
ILexV <iav.fenix.net@gmail.com>
2026-01-29 16:49:16 +0600

    fix og:image

--- MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
+++ MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for MyDevTools PWA
-const CACHE_VERSION = 'v1.0.0-1769682940447';
+const CACHE_VERSION = 'v1.0.0-1769683338478';
 const CACHE_NAME = `mydevtools-${CACHE_VERSION}`;
 
 // Assets to cache on install

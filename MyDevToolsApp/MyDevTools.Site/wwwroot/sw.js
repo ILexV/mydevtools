22234c6cef0f86d63a600ae81a66eedc00049e6c
ILexV <iav.fenix.net@gmail.com>
2026-02-13 16:09:33 +0600

    fix

--- MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
+++ MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for MyDevTools PWA
-const CACHE_VERSION = 'v1.0.0-1770913218908';
+const CACHE_VERSION = 'v1.0.0-1770977248860';
 const CACHE_NAME = `mydevtools-${CACHE_VERSION}`;
 
 // Assets to cache on install

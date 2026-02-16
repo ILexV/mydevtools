c047d1851c907a6e198fc90ed292db2a3b99e1ef
ILexV <iav.fenix.net@gmail.com>
2026-02-16 22:23:38 +0600

    доработана локализация главной страницы

--- MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
+++ MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for MyDevTools PWA
-const CACHE_VERSION = 'v1.0.0-1771257257782';
+const CACHE_VERSION = 'v1.0.0-1771258963605';
 const CACHE_NAME = `mydevtools-${CACHE_VERSION}`;
 
 // Assets to cache on install

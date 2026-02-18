88325d5a1790e29cd47d32a8351462cd9a342da9
ILexV <iav.fenix.net@gmail.com>
2026-02-18 22:22:14 +0600

    доработка локализации

--- MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
+++ MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for MyDevTools PWA
-const CACHE_VERSION = 'v1.0.0-1771266158335';
+const CACHE_VERSION = 'v1.0.0-1771431576245';
 const CACHE_NAME = `mydevtools-${CACHE_VERSION}`;
 
 // Assets to cache on install

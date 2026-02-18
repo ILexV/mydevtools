16ae3e29ff3db24a2e748956ede292f7faa38079
ILexV <iav.fenix.net@gmail.com>
2026-02-18 23:20:24 +0600

    доработка кнопки начать

--- MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
+++ MyDevToolsApp/MyDevTools.Site/wwwroot/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for MyDevTools PWA
-const CACHE_VERSION = 'v1.0.0-1771434600200';
+const CACHE_VERSION = 'v1.0.0-1771435098848';
 const CACHE_NAME = `mydevtools-${CACHE_VERSION}`;
 
 // Assets to cache on install
